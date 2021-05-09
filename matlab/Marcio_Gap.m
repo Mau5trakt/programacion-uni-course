1688daa68cbd68ba8d4324c8f3dd077a47ab3cbb
Mau5trakt <80871251+Mau5trakt@users.noreply.github.com>
2021-05-08 22:55:06 -0600

    nada

--- matlab/Marcio_Gap.m
+++ matlab/Marcio_Gap.m
@@ -1,14 +1,19 @@
-%Mickey Thompson Like's purru 
-Re = input('Ingrese el Reynolds \n');
+% Programa para calcular el punto de ebullicion del agua 
+% a distintas temperaturas o Presiones
+% Hecho por Enrique Mauricio Alemany Torres
 
-if 0 < Re && Re <= 2400
-    disp('Regimen Laminar')
-elseif 2400 < Re && Re < 4000
-    disp('Regimen Transitorio')
-elseif Re >= 4000 
-    disp('Regimen Turbulento')
+T = input('Ingrese la temperatura en °C /n');
+if 0 < T && T < 100
+    A = 8.07131;
+    B = 1730.63;
+    C = 233.426;
+elseif 100 <= T && T <= 374
+    A = 8.14019;
+    B = 1810.94;
+    C = 244.485;
 else 
-    disp('El valor de Reynolds no puede ser 0 o negativo')
+    disp('Temperatura fuera de rango, ingrese una temperatura entre 0 y 374°C')
 end
 
-%%
+P = 10^(A-B/(T+C));
+fprintf('La presión de ebullucion para T = %6.2f °C es de %6.3f bar',T,P)
