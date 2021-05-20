5746c14fb0e836e7560c27c45fa4d3f7b92c4ff6
Mau5trakt <kubzalemany@hotmail.com>
2021-05-20 15:09:33 -0600

    watprop

---
+++ matlab/watprop.m
@@ -0,0 +1,16 @@
+% Primer columna Temp, Segunda Densidad, Tercera Viscosidad la cual debe
+% dividirse entre 1000 para q de las unidades correctas
+
+wp=[0	999.9	1.792;
+    5	1000	1.519;
+    10	999.7	1.308;
+    20	998.2	1.005;
+    30  995.7   0.785;
+    40	992.2	0.656;
+    50  988.1   0.522;
+    60	983.2	0.469;
+    70  977.7   0.418;
+    80	971.8	0.357;
+    90  965.2   0.341;
+    100	958.4	0.284];
+
